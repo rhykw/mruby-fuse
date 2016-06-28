45f86e87849ce6062c6e1fcc56ca8b7ca512ad35
rhykw <dev@rhykw.net>
2016-06-28 20:29:06 +0900

    fuse_mount動かしてみた

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,6 +1,10 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
+
   conf.gembox 'default'
-  conf.gem '../../mruby-fuse'
+  conf.gem '../../mruby-fuse' do |g|
+    g.cc.flags << '-g -D_FILE_OFFSET_BITS=64'
+    g.linker.flags << '-lfuse'
+  end
   conf.enable_test
 end
--- src/mrb_fuse.c
+++ src/mrb_fuse.c
@@ -7,12 +7,15 @@
 */
 
 #define _GNU_SOURCE 1
+#define FUSE_USE_VERSION 26
 
 #include <stdio.h>
 #include <sched.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fuse/fuse.h>
 
 #include "mruby.h"
 #include "mruby/data.h"
@@ -31,6 +34,10 @@ static const struct mrb_data_type mrb_fuse_data_type = {
 
 static mrb_value mrb_fuse_init(mrb_state *mrb, mrb_value self)
 {
+  const char *path = NULL;
+  int path_len = 0;
+
+  int r;
   mrb_fuse_data *data;
 
   data = (mrb_fuse_data *)DATA_PTR(self);
@@ -40,6 +47,43 @@ static mrb_value mrb_fuse_init(mrb_state *mrb, mrb_value self)
   DATA_TYPE(self) = &mrb_fuse_data_type;
   DATA_PTR(self) = NULL;
 
+  const char *option = NULL;
+  int option_len = 0;
+
+  mrb_get_args(mrb, "z|z", &path, &option);
+
+  /* path_len = strlen(path); */
+  option_len = strlen(option);
+
+#if 1
+
+  struct fuse_operations op;
+  memset(&op, 0, sizeof(struct fuse_operations));
+
+  int argc = 2;
+  if (option && option_len > 0) {
+    argc += 2;
+  }
+
+  char **argv = malloc(sizeof(char*)*argc);
+
+  char *p = strdup(path);
+  char *q = strdup(option);
+
+  int i = 0;
+  *(argv + i++) = "mruby-fuse";
+  if (option && option_len > 0) {
+    *(argv + i++) = "-o";
+    *(argv + i++) = q;
+  }
+  *(argv + i++) = p;
+
+  r = fuse_main(argc, argv, &op, NULL);
+
+  printf("r=%d\n", r);
+
+#endif
+
   return self;
 }
 
@@ -48,7 +92,7 @@ void mrb_mruby_fuse_gem_init(mrb_state *mrb)
   struct RClass *fuse;
   fuse = mrb_define_class(mrb, "Fuse", mrb->object_class);
 
-  mrb_define_method(mrb, fuse, "initialize", mrb_fuse_init, MRB_ARGS_NONE());
+  mrb_define_method(mrb, fuse, "initialize", mrb_fuse_init, MRB_ARGS_REQ(1)|MRB_ARGS_OPT(1));
 
   DONE;
 }
